76f34d7a330a04504431e722370a8d0dc27fa136
Vika Aleksandrova <viktoriya_job@mail.ru>
2023-11-12 15:12:50 +0300

    Homework1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Lesson1{
+   class Progrsm {
+      static void Main() {
+         Console.Write("Введите свое имя: ");
+         string user_name = Console.ReadLine();
+         Console.WriteLine("Hello, " + user_name);
+      }
+   }
+
+}
+
+
